4ec75c37ed27670fb94ee84e3ec5e115efe145d2
mattmc3 <mattmc3@gmail.com>
2019-07-15 13:30:50 -0400

    Switch back to antibody

--- conf.d/20_plugins.zsh
+++ conf.d/20_plugins.zsh
@@ -1,52 +1,58 @@
-# ### antibody {{{
-# source <(antibody init)
-# antibody bundle < $ZDOTDIR/conf.d/20_antibody_plugins.txt
+### antibody {{{
 
-# # post-activities after plugins load
+# omz themes I like:
+# avit, juanghurtado, lambda, refined, steeef, terminalparty, wezm
+ZSH_THEME=${ZSH_THEME:-refined}
 
-# # to handle autosuggest fg=8 blending with terminal background
-# export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=60'
+source <(antibody init)
+antibody bundle < $ZDOTDIR/conf.d/20_antibody_plugins.txt
 
-# # spaceship prompt config
-# # https://github.com/denysdovhan/spaceship-prompt
-# # https://github.com/denysdovhan/spaceship-prompt/blob/master/docs/Options.md
-# SPACESHIP_CHAR_SYMBOL='%% '
-# ### }}}
+# post-activities after plugins load
+
+# to handle autosuggest fg=8 blending with terminal background
+export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=60'
+
+# spaceship prompt config
+# https://github.com/denysdovhan/spaceship-prompt
+# https://github.com/denysdovhan/spaceship-prompt/blob/master/docs/Options.md
+SPACESHIP_CHAR_SYMBOL='%% '
+### }}}
 
 
 ### antigen {{{
-export ADOTDIR="$ZDOTDIR"/.antigen
-if [[ ! -f "$ADOTDIR"/antigen.zsh ]]; then
-  mkdir -p "$ADOTDIR"
-  curl -L git.io/antigen > "$ADOTDIR"/antigen.zsh
-fi
-source "$ADOTDIR"/antigen.zsh
+# export ADOTDIR="$ZDOTDIR"/.antigen
+# if [[ ! -f "$ADOTDIR"/antigen.zsh ]]; then
+#   mkdir -p "$ADOTDIR"
+#   curl -L git.io/antigen > "$ADOTDIR"/antigen.zsh
+# fi
+# source "$ADOTDIR"/antigen.zsh
 
-# Load the oh-my-zsh's library.
-antigen use oh-my-zsh
+# # Load the oh-my-zsh's library.
+# antigen use oh-my-zsh
 
-# Load the theme
-# https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
-# antigen theme https://github.com/denysdovhan/spaceship-zsh-theme spaceship
-antigen theme terminalparty
+# # Load the theme
+# # https://github.com/robbyrussell/oh-my-zsh/wiki/Themes
+# # antigen theme https://github.com/denysdovhan/spaceship-zsh-theme spaceship
+# # Themes I like: refined, terminalparty, avit, juanghurtado, lambda, steeef, wezm
+# antigen theme wezm
 
-# in case anything can take advantage of async
-antigen bundle mafredri/zsh-async
+# # in case anything can take advantage of async
+# antigen bundle mafredri/zsh-async
 
-# Bundles from the default repo (robbyrussell's oh-my-zsh).
-antigen bundle colored-man-pages
-antigen bundle extract
-antigen bundle python
+# # Bundles from the default repo (robbyrussell's oh-my-zsh).
+# antigen bundle colored-man-pages
+# antigen bundle extract
+# antigen bundle python
 
-antigen bundle rupa/z
-antigen bundle zsh-users/zsh-completions
-antigen bundle zsh-users/zsh-autosuggestions
+# antigen bundle rupa/z
+# antigen bundle zsh-users/zsh-completions
+# antigen bundle zsh-users/zsh-autosuggestions
 
-# these should be at last!
-# antigen bundle sindresorhus/pure
-antigen bundle zdharma/fast-syntax-highlighting
-antigen bundle zsh-users/zsh-history-substring-search
+# # these should be at last!
+# # antigen bundle sindresorhus/pure
+# antigen bundle zdharma/fast-syntax-highlighting
+# antigen bundle zsh-users/zsh-history-substring-search
 
-# Tell Antigen that you're done.
-antigen apply
-### }}}
+# # Tell Antigen that you're done.
+# antigen apply
+# ### }}}
