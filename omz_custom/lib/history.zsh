4423d97550141787ce1e4cfa1f97706ab2b9e65d
mattmc3 <mattmc3@gmail.com>
2019-07-22 14:24:57 -0400

    Store history in XDG data

--- omz_custom/lib/history.zsh
+++ omz_custom/lib/history.zsh
@@ -1,5 +1,9 @@
 source "$ZSH"/lib/history.zsh
 
-HISTFILE="${ZDOTDIR:-$HOME}/.zsh_history"
+if [[ -n $XDG_DATA_HOME ]]; then
+  HISTFILE="$XDG_DATA_HOME"/zsh/zsh_history
+else
+  HISTFILE="${ZDOTDIR:-$HOME}/.zsh_history"
+fi
 HISTSIZE=100000
 SAVEHIST=100000
