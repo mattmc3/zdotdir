d99bb530db11c4f33b37a7f1bec5e3ffef0f21d8
mattmc3 <mattmc3@gmail.com>
2019-10-07 11:49:42 -0400

    Fix local

--- conf.d/local.zsh
+++ conf.d/local.zsh
@@ -1,2 +1,6 @@
 ### local ------------------------------------------------------------------ {{{
-[[ -f "${ZDOTDIR:-HOME}"/.zshrc.local ]] && . "${ZDOTDIR:-HOME}"/.zshrc.local
+if [[ -f "${ZDOTDIR:-HOME}"/.zshrc.local ]]; then
+  . "${ZDOTDIR:-HOME}"/.zshrc.local
+elif [[ -f "${DOTFILES}"/local/zsh/zsh/zshrc.local.zsh ]]; then
+  . "${DOTFILES}"/local/zsh/zsh/zshrc.local.zsh
+fi
