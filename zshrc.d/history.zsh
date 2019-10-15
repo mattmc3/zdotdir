2ad6acb0004c0f65bf52e3e91371663d00ca9c45
mattmc3 <mattmc3@gmail.com>
2019-10-14 20:45:09 -0400

    Cleanup history

--- zshrc.d/history.zsh
+++ zshrc.d/history.zsh
@@ -1,13 +1,4 @@
 ### history ---------------------------------------------------------------- {{{
-# options: http://zsh.sourceforge.net/Doc/Release/Options.html#History
-
-# set for OMZ history lib
-HIST_STAMPS="yyyy-mm-dd"
-
-# omz
-# [[ -f $ZSH/lib/history.zsh ]] && . $ZSH/lib/history.zsh
-# [[ -f $ZSH/plugins/history/history.plugin.zsh ]] && . $ZSH/plugins/history/history.plugin.zsh
-
 # fix OMZ settings to be better
 SAVEHIST=100000
 HISTSIZE=50000
