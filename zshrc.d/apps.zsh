808301c2137e125cfea5040d2c8979a73c84cec0
mattmc3 <mattmc3@gmail.com>
2019-12-04 12:27:07 -0500

    Setup Java support for pentaho

--- zshrc.d/apps.zsh
+++ zshrc.d/apps.zsh
@@ -82,7 +82,9 @@ alias autorotate="jhead -autorot"
 ### java ------------------------------------------------------------------- {{{
 if [[ "$OSTYPE" == darwin* ]]; then
   # run this: /usr/libexec/java_home -v 10
-  export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-10.0.2.jdk/Contents/Home"
+  # export JAVA_HOME="/Library/Java/JavaVirtualMachines/jdk-10.0.2.jdk/Contents/Home"
+  export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
+  export PENTAHO_JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
 
   # alias j12="export JAVA_HOME=\`/usr/libexec/java_home -v 12\`; java -version"
   # alias j11="export JAVA_HOME=\`/usr/libexec/java_home -v 11\`; java -version"
