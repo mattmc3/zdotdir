0056598380126cca681b7ec644fecd2432cfe626
mattmc3 <mattmc3@gmail.com>
2020-09-22 16:44:05 -0400

    ZSH fix for moved local zshrc

--- zlib/local.zsh
+++ zlib/local.zsh
@@ -1,5 +1,6 @@
 ### local ------------------------------------------------------------------ {{{
 # make sure that the dotfiles .zshrc.local exists
-if [[ ! -f "${ZDOTDIR:-HOME}"/.zshrc.local ]] && [[ -f "${DOTFILES}"/local/zsh/zsh/zshrc.local.zsh ]]; then
-  ln -s "${DOTFILES}"/local/zsh/zsh/zshrc.local.zsh "${ZDOTDIR:-HOME}"/.zshrc.local
+if [[ ! -f "${ZDOTDIR:-HOME}"/.zshrc.local ]] && [[ -f "${DOTFILES}"/local/zsh/zshrc.local.zsh ]]; then
+  ln -s "${DOTFILES}"/local/zsh/zshrc.local.zsh "${ZDOTDIR:-HOME}"/.zshrc.local
 fi
+[[ -f "${ZDOTDIR:-HOME}"/.zshrc.local ]] && . "${ZDOTDIR:-HOME}"/.zshrc.local
