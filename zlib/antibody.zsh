ea835f8edcc72ed3031533dd6f5d05f757e920d3
mattmc3 <mattmc3@gmail.com>
2020-09-22 12:05:04 -0400

    :bug: Fix plugins

--- zlib/antibody.zsh
+++ zlib/antibody.zsh
@@ -6,6 +6,9 @@
 
 path=( /usr/local/{sbin,bin} $path )
 zplugins_file="$ZDOTDIR/zlib/zplugins.zsh"
+if [[ ! -d "$ANTIBODY_HOME" ]] && [[ -f "$zplugins_file" ]]; then
+  rm -f "$zplugins_file"
+fi
 if [[ ! -f "$zplugins_file" ]] || [[ $ZDOTDIR/.zplugins -nt "$zplugins_file" ]]; then
   envsubst < $ZDOTDIR/.zplugins | antibody bundle >| $zplugins_file
   sed -i '' "s|$HOME|\$HOME|g" $zplugins_file
--- zlib/zplugins.zsh
+++ zlib/zplugins.zsh
@@ -5,11 +5,17 @@ fpath+=( $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-z
 source $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-mattmc3-SLASH-zsh-compinit/zsh-compinit.plugin.zsh
 fpath+=( $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-mattmc3-SLASH-zsh-compinit )
 source $HOME/.config/zsh/zlib/environment.zsh
+source $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async/async.plugin.zsh
+fpath+=( $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async )
 source $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/clipboard.zsh
 source $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/termsupport.zsh
 source $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/lib/key-bindings.zsh
-source $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async/async.plugin.zsh
-fpath+=( $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-mafredri-SLASH-zsh-async )
+source $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/osx/osx.plugin.zsh
+fpath+=( $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/osx )
+source $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/extract/extract.plugin.zsh
+fpath+=( $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/extract )
+source $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/colored-man-pages/colored-man-pages.plugin.zsh
+fpath+=( $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/colored-man-pages )
 source $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
 fpath+=( $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
 source $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-completions/zsh-completions.plugin.zsh
@@ -22,16 +28,10 @@ source $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-zsh
 fpath+=( $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-zshzoo-SLASH-zfunctions )
 source $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-mattmc3-SLASH-zsh-cd-ls/zsh-cd-ls.plugin.zsh
 fpath+=( $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-mattmc3-SLASH-zsh-cd-ls )
-source $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/osx/osx.plugin.zsh
-fpath+=( $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/osx )
-source $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/extract/extract.plugin.zsh
-fpath+=( $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/extract )
-source $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/colored-man-pages/colored-man-pages.plugin.zsh
-fpath+=( $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-ohmyzsh-SLASH-ohmyzsh/plugins/colored-man-pages )
-source $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-rupa-SLASH-z/z.sh
-fpath+=( $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-rupa-SLASH-z )
 source $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-mattmc3-SLASH-zsh-safe-rm/zsh-safe-rm.plugin.zsh
 fpath+=( $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-mattmc3-SLASH-zsh-safe-rm )
+source $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-rupa-SLASH-z/z.sh
+fpath+=( $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-rupa-SLASH-z )
 source $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure/pure.plugin.zsh
 fpath+=( $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-sindresorhus-SLASH-pure )
 source $HOME/.config/zsh/zplugins/https-COLON--SLASH--SLASH-github.com-SLASH-zshzoo-SLASH-zshrc.d/zshrc.d.plugin.zsh
