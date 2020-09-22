4d1f9d19bc247e8afcf79093aa728be221fcacf7
mattmc3 <mattmc3@gmail.com>
2020-09-22 11:20:09 -0400

    Rename .zsh_plugins to .zplugins

--- zshrc.d/antibody.zsh
+++ zshrc.d/antibody.zsh
@@ -1,6 +1,6 @@
 export ANTIBODY_HOME="$ZDOTDIR/zplugins"
 if [[ ! -f "$ZDOTDIR/zshrc.d/zsh_plugins.zsh" ]]; then
- envsubst < $ZDOTDIR/.zsh_plugins | antibody bundle > $ZDOTDIR/zshrc.d/zsh_plugins.zsh
+ envsubst < $ZDOTDIR/.zplugins | antibody bundle > $ZDOTDIR/zshrc.d/zsh_plugins.zsh
  sed -i '' "s|$HOME|\$HOME|g" $ZDOTDIR/zshrc.d/zsh_plugins.zsh
  source $ZDOTDIR/zshrc.d/zsh_plugins.zsh
 fi
