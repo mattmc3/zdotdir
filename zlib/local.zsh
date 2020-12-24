35d47afb096edc72c040c24dc5e5d8528af75914
mattmc3 <mattmc3@gmail.com>
2020-12-24 18:20:08 -0500

    Remove zlib

--- zlib/local.zsh
+++
@@ -1,6 +0,0 @@
-### local ------------------------------------------------------------------ {{{
-# make sure that the dotfiles .zshrc.local exists
-if [[ ! -f "${ZDOTDIR:-HOME}"/.zshrc.local ]] && [[ -f "${DOTFILES}"/local/zsh/zshrc.local.zsh ]]; then
-  ln -s "${DOTFILES}"/local/zsh/zshrc.local.zsh "${ZDOTDIR:-HOME}"/.zshrc.local
-fi
-[[ -f "${ZDOTDIR:-HOME}"/.zshrc.local ]] && . "${ZDOTDIR:-HOME}"/.zshrc.local
