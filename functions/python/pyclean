52c12852463abc4f4761c9a32dfaad21510aebf2
mattmc3 <mattmc3@gmail.com>
2021-09-12 21:42:43 -0400

    Move python functions to zshrc.d

--- functions/python/pyclean
+++
@@ -1,2 +0,0 @@
-find . -type f -name "*.py[co]" -delete
-find . -type d -name "__pycache__" -delete
--- zshrc.d/python.zsh
+++ zshrc.d/python.zsh
@@ -1,6 +1,6 @@
 if [[ -z $WORKON_HOME ]]; then
   export WORKON_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"/venvs
-  [[ -d "$WORKON_HOME" ]] || { mkdir -p "$WORKON_HOME" }
+  [[ -d "$WORKON_HOME" ]] || mkdir -p "$WORKON_HOME"
 fi
 
 alias py2='python2'
@@ -11,6 +11,32 @@ alias pip3update="pip3 list --outdated | cut -d ' ' -f1 | xargs -n1 pip3 install
 alias pyfind='find . -name "*.py"'
 alias pygrep='grep --include="*.py"'
 alias pyva="source .venv/bin/activate"
+
+function pyclean {
+  find . -type f -name "*.py[co]" -delete
+  find . -type d -name "__pycache__" -delete
+}
+
+function workon {
+  if [[ -z "$1" ]]; then
+    >&2 echo "Expecting workon project name" && return 1
+  elif [[ -z "$WORKON_HOME" ]]; then
+    >&2 echo "\$WORKON_HOME not set" && return 1
+  elif [[ ! -d "$WORKON_HOME" ]]; then
+    >&2 echo "\$WORKON_HOME directory not found: $WORKON_HOME" && return 1
+  fi
+  source "$WORKON_HOME/$1/bin/activate"
+}
+
+function mkvenv {
+  if [[ -n $1 ]]; then
+    >&2 echo "mkvenv: Usage - mkvenv <venv-name>" && return 1
+  elif [[ -z $WORKON_HOME ]]; then
+    >&2 echo "mkvenv: \$WORKON_HOME not set" && return 1
+  fi
+  python3 -m venv "${WORKON_HOME}\${1}"
+  workon "${1}"
+}
 
 # python: tab complete for workon dir (virtualenv)
 compdef '_files -W "$WORKON_HOME"' workon &> /dev/null
