e6f090af95c45cda0fbd16c120be389657aa7dad
mattmc3 <mattmc3@gmail.com>
2021-09-12 23:09:20 -0400

    Clean up autoload_funcdir

--- zshrc.d/zfunctions.zsh
+++ zshrc.d/zfunctions.zsh
@@ -1,4 +1,14 @@
 # autoload functions
+function autoload_funcdir {
+  if [[ ! -d "$1" ]]; then
+    >&2 echo "Function directory not found: $1" && return 1
+  fi
+  fpath=("$1" $fpath)
+  local fn; for fn in "$1"/*(.N); do
+    autoload -Uz "$fn"
+  done
+}
+
 fndirs=(
   ${ZDOTDIR:-$HOME}/.zfunctions
   ${ZDOTDIR:-$HOME}/.zfunctions/*/(N)
@@ -9,9 +19,6 @@ fndirs=(
 )
 for d in $fndirs; do
   [[ -d $d ]] || continue
-  fpath=("$d" $fpath)
-  for fn in "$d"/*(.N); do
-    autoload -Uz "$fn"
-  done
+  autoload_funcdir "$d"
 done
-unset d fn fndirs
+unset fndirs d
