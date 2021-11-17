b2c1b24d1473e7b9b2089aef57cca0b841232184
mattmc3 <mattmc3@gmail.com>
2021-11-17 16:17:25 -0500

    Remove omz

--- conf.d/01-prezto.zsh
+++ conf.d/01-prezto.zsh
@@ -1,9 +1,7 @@
 ### plugin variables -------------------------------------------------------------------
-PURE_PROMPT_SYMBOL="%%"
-MAGIC_ENTER_OTHER_COMMAND="ls -GF"
-MAGIC_ENTER_GIT_COMMAND="ls -GF && git status -s"
+ZPLUGINDIR="$ZDOTDIR/plugins"
 
-external_plugins=(
+prezto_plugins=(
   # prezto first!
   sorin-ionescu/prezto
   # contribs next
@@ -12,14 +10,6 @@ external_plugins=(
   zdharma-continuum/fast-syntax-highlighting
 )
 
-omz_plugins=(
-  extract
-  # magic-enter
-  # colored-man-pages
-)
-
-
-### prezto framework -------------------------------------------------------------------
 # set prezto variables
 ZPREZTODIR="$ZPLUGINDIR/prezto"
 # prezto doesn't set this variable, so borrow the omz one
@@ -28,7 +18,7 @@ ZSH_COMPDUMP="${XDG_CACHE_HOME:-$HOME/.cache}/prezto/zcompdump"
 # clone prezto and contribs
 () {
   local REPLY repo plugin_name initfile
-  for repo in $external_plugins; do
+  for repo in $prezto_plugins; do
     plugin_name="${repo:t}"
     plugin-clone https://github.com/${repo}.git
     if [[ ! -e $ZPLUGINDIR/$plugin_name/init.zsh ]]; then
@@ -46,13 +36,3 @@ setopt emacs                  # use emacs keybindings in the shell
 setopt glob_dots              # include dotfiles when globbing
 setopt emacs                  # use emacs keybindings in the shell
 setopt pushd_minus            # exchanges the meanings of ‘+’ and ‘-’ when specifying a directory in the stack
-
-
-### ohmyzsh variables and plugins ------------------------------------------------------
-ZSH="$ZPLUGINDIR/ohmyzsh"
-ZSH_CUSTOM="$ZPLUGINDIR/ohmyzsh/custom"
-
-for plugin in $omz_plugins; do
-  source $ZSH/plugins/$plugin/$plugin.plugin.zsh
-done
-unset plugin
