3035cc10917672728bed47b5e91653b4f0b73d9e
mattmc3 <mattmc3@gmail.com>
2021-11-29 13:55:19 -0500

    Better local sourcing

--- zshrc.d/ZZ-local.zsh
+++ zshrc.d/ZZ-local.zsh
@@ -1,2 +1,6 @@
 # local overrides
-[[ -f $DOTFILES.local/zsh/zshrc.local.zsh ]] && . $DOTFILES.local/zsh/zshrc.local.zsh
+if [[ ! -f $ZDOTDIR/zshrc.local.zsh ]] && [[ -f $DOTFILES.local/zsh/zshrc.local.zsh ]]; then
+  ln -s $DOTFILES.local/zsh/zshrc.local.zsh $ZDOTDIR/zshrc.local.zsh
+fi
+
+[[ -f $ZDOTDIR/zshrc.local.zsh ]] && . $ZDOTDIR/zshrc.local.zsh
