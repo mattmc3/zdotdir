1a6d8bc1298b905a99ca7ea97cf7440f5c6f3c8f
mattmc3 <mattmc3@gmail.com>
2021-12-11 11:19:31 -0500

    Add manpage coloring

---
+++ zshrc.d/manpage-colors.zsh
@@ -0,0 +1,9 @@
+# Coloured man page support
+# using 'less' env vars (format is '\E[<brightness>;<colour>m')
+export LESS_TERMCAP_mb=$'\E[01;31m'      # Begins blinking.
+export LESS_TERMCAP_md=$'\E[01;31m'      # Begins bold.
+export LESS_TERMCAP_me=$'\E[0m'          # Ends mode.
+export LESS_TERMCAP_se=$'\E[0m'          # Ends standout-mode.
+export LESS_TERMCAP_so=$'\E[00;47;30m'   # Begins standout-mode.
+export LESS_TERMCAP_ue=$'\E[0m'          # Ends underline.
+export LESS_TERMCAP_us=$'\E[01;32m'      # Begins underline.
