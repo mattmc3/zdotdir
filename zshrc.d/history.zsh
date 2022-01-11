e9402f9b6cc598d60ec46488ee9dee906cc64e2b
mattmc3 <mattmc3@gmail.com>
2022-01-11 08:21:46 -0500

    Change history options

--- zshrc.d/history.zsh
+++ zshrc.d/history.zsh
@@ -4,8 +4,11 @@ HISTFILE="${XDG_DATA_HOME:-$HOME/.local/share}/zsh/history"
 SAVEHIST=20000
 HISTSIZE=100000
 
-setopt hist_no_store          # don't store history commands
-setopt hist_reduce_blanks     # remove superfluous blanks from each command line being added to the history list
+setopt hist_no_store            # don't store history commands
+setopt hist_reduce_blanks       # remove superfluous blanks from each command line being added to the history list
+setopt no_share_history         # allow each session to have its own history
+setopt inc_append_history       # add commands to the history file as they are run
+setopt inc_append_history_time  # add time command ran to history
 
 alias history="fc -li"        # make the history command more useful
 
