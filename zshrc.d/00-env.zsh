1eaa685df0afbf74b94a0e8fa4c57ffd3226b2c0
mattmc3 <mattmc3@gmail.com>
2022-01-25 08:35:00 -0500

    Move environment vars

--- zshrc.d/00-env.zsh
+++ zshrc.d/00-env.zsh
@@ -67,3 +67,5 @@ export ZPLUGINDIR=${ZPLUGINDIR:-${ZDOTDIR:-~}/.zplugins}
 # set better plugin values
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=8'
 PURE_PROMPT_SYMBOL="%%"
+MAGIC_ENTER_GIT_COMMAND="git status -sb"
+MAGIC_ENTER_OTHER_COMMAND="ls -G"
