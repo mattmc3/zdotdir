f811d458d6df56851444fe74d5eb06f426e4e2f6
mattmc3 <mattmc3@gmail.com>
2022-01-29 23:04:29 -0500

    Change plugin handler

--- zshrc.d/01-plugins.zsh
+++ zshrc.d/01-plugins.zsh
@@ -3,12 +3,12 @@
 ZPLUGINDIR=${ZPLUGINDIR:-${ZDOTDIR:-~}/.zplugins}
 path=($ZDOTDIR/bin $path)
 
-if [[ ! -f $ZPLUGINDIR/init.zsh ]]; then
-  mkdir -p $ZPLUGINDIR
-  envsubst < ${ZDOTDIR:-~}/.zsh_plugins.txt | plugin bundle >| $ZPLUGINDIR/init.zsh
-  sed -i '' "s|$HOME|\$HOME|g" $ZPLUGINDIR/init.zsh
-fi
-source $ZPLUGINDIR/init.zsh
+# if [[ ! -f $ZPLUGINDIR/init.zsh ]]; then
+#   mkdir -p $ZPLUGINDIR
+#   envsubst < ${ZDOTDIR:-~}/.zpluginrc | plugin bundle >| $ZPLUGINDIR/init.zsh
+#   sed -i '' "s|$HOME|\$HOME|g" $ZPLUGINDIR/init.zsh
+# fi
+plugin loadall
 
 #ABBR_USER_ABBREVIATIONS_FILE=$ZDOTDIR/.zabbrs
 MAGIC_ENTER_GIT_COMMAND="git status -sb"
--- zshrc.d/abbr.zsh
+++
@@ -1,3 +0,0 @@
-if [[ -f $ZPLUGINDIR/zsh-abbr/zsh-abbr.plugin.zsh ]]; then
-  zsh-defer source $ZPLUGINDIR/zsh-abbr/zsh-abbr.plugin.zsh
-fi
