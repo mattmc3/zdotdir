8a24a9c971c29484f44547abf69bfb9410ae8930
mattmc3 <mattmc3@gmail.com>
2022-06-30 18:00:17 -0400

    Move z config to plugins

--- conf.d/apps.zsh
+++ conf.d/apps.zsh
@@ -161,8 +161,5 @@ alias todos="$VISUAL $HOME/Desktop/todo.txt"
 export WGETRC="$XDG_CONFIG_HOME/wget/wgetrc"
 alias wget='wget --hsts-file="$XDG_CACHE_HOME/wget/wget-hsts"'
 
-# z
-export _Z_DATA="$XDG_DATA_HOME/z/data"
-
 # clear dupes
 typeset -gU cdpath fpath mailpath path
