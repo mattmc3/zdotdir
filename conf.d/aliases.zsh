71be6b5b8cd152513ede59eb9139ff7feb743ba8
mattmc3 <mattmc3@gmail.com>
2022-07-27 16:38:06 -0400

    Refactor aliases

--- conf.d/aliases.zsh
+++ conf.d/aliases.zsh
@@ -1,113 +1,103 @@
-### aliases.zsh
+#
+# aliases
+#
 
 # https://medium.com/@webprolific/getting-started-with-dotfiles-43c3602fd789#.vh7hhm6th
 # https://github.com/webpro/dotfiles/blob/master/system/.alias
 # https://github.com/mathiasbynens/dotfiles/blob/master/.aliases
 # https://github.com/ohmyzsh/ohmyzsh/blob/master/plugins/common-aliases/common-aliases.plugin.zsh
 
-alias 1='cd -'
-alias 2='cd -2'
-alias 3='cd -3'
-alias 4='cd -4'
-alias 5='cd -5'
-alias 6='cd -6'
-alias 7='cd -7'
-alias 8='cd -8'
-alias 9='cd -9'
-
-alias cd..='cd ..'
-alias -g ..2='../..'
-alias -g ..3='../../..'
-alias -g ..4='../../../..'
-alias -g ..5='../../../../..'
-alias -g ...='../..'
-alias -g ....='../../..'
-alias -g .....='../../../..'
-alias -g ......='../../../../..'
-
-alias zshrc='$VISUAL "${ZDOTDIR:-$HOME}"/.zshrc'
-alias zbench='for i in $(seq 10); do; /usr/bin/time zsh -lic exit; done'
-alias zprofrc="ZPROFRC=1 zsh"
-alias zdot='cd $ZDOTDIR'
-alias confd='cd $HOME/.config'
-alias proj='cd $HOME/Projects'
-
-# zsh pipes
+# zsh suffix aliases
 alias -g H='| head'
 alias -g T='| tail'
 alias -g G='| grep -E'
 alias -g S='| sort'
 alias -g L='| less'
 alias -g M='| more'
+alias -g ..2='../..'
+alias -g ..3='../../..'
+alias -g ..4='../../../..'
+alias -g ..5='../../../../..'
+alias -g ..6='../../../../../..'
+alias -g ..7='../../../../../../..'
+alias -g ..8='../../../../../../../..'
+alias -g ..9='../../../../../../../../..'
+alias -g ...='../..'
+alias -g ....='../../..'
+alias -g .....='../../../..'
+alias -g ......='../../../../..'
+
+# single character shortcuts - be sparing!
+alias _=sudo
+alias d='dirs -v'
+alias l=ls
+
+# mask built-ins with better defaults
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='rm -i'
+alias mkdir='mkdir -p'
+alias ping='ping -c 5'
+alias type='type -a'
+alias vi=vim
+if [[ "$OSTYPE" == darwin* ]]; then
+  alias ls="ls -G"
+else
+  alias ls="ls --color=auto"
+fi
+alias grep="grep --color=auto --exclude-dir={CVS,.git,.hg,.svn}"
+
+# more ways to ls
+alias ll='ls -lh'
+alias la='ls -lAh'
+alias ldot='ls -ld .*'
+
+# fix typos
+alias quit='exit'
+alias cd..='cd ..'
+
+# tar
+alias tarls="tar -tvf"
+alias untar="tar -xf"
+
+# date/time
+alias timestamp="date '+%Y-%m-%d %H:%M:%S'"
+alias datestamp="date '+%Y-%m-%d'"
+
+# find
+alias fd='find . -type d -name'
+alias ff='find . -type f -name'
+
+# disk usage
+alias biggest='du -s ./* | sort -nr | awk '\''{print $2}'\'' | xargs du -sh'
+alias dux='du -x --max-depth=1 | sort -n'
+alias dud='du -d 1 -h'
+alias duf='du -sh *'
+
+# url encode/decode
+alias urldecode='python3 -c "import sys, urllib.parse as ul; \
+    print(ul.unquote_plus(sys.argv[1]))"'
+alias urlencode='python3 -c "import sys, urllib.parse as ul; \
+    print (ul.quote_plus(sys.argv[1]))"'
+
+# history
+# list the ten most used commands
+alias history-stat="history 0 | awk '{print \$2}' | sort | uniq -c | sort -n -r | head"
+alias history="fc -li"
+
+# misc
+alias please=sudo
+alias zshrc='${EDITOR:-vim} "${ZDOTDIR:-$HOME}"/.zshrc'
+alias zbench='for i in {1..10}; do /usr/bin/time zsh -lic exit; done'
+alias zdot='cd $ZDOTDIR'
 
 # echo things
 alias print-fpath='for fp in $fpath; do echo $fp; done; unset fp'
 alias print-path='echo $PATH | tr ":" "\n"'
 alias print-functions='print -l ${(k)functions[(I)[^_]*]} | sort'
 
-# mask built-ins with better defaults
-alias cp='cp -i'
-alias mv='mv -i'
-#alias rm='rm -i'
-alias ls='ls -G'
-alias mkdir="mkdir -p"
-alias ping='ping -c 5'
-alias type="type -a"
-
-# single character shortcuts - be sparing!
-alias _='sudo'
-
-# shortcuts
-alias vi='vim'
-alias nv='nvim'
-alias afind='ack -il'
-alias md='mkdir -p'
-alias rd=rmdir
-alias please=sudo
-alias po=popd
-alias pu=pushd
-alias globurl='noglob urlglobber '
-alias zz=exit
-
-# more ways to ls
-alias ll='ls -lFh'
-alias l='ls -F'
-alias la='ls -lAFh'
-alias ldot='ls -ld .*'
-
-# fix typos
-alias quit='exit'
-
-# tools
-alias te='${EDITOR:-vim}'
-alias ide='${VISUAL:-vim}'
-
-# network
-alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
-alias iplocal="ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'"
-alias speedtest="wget -O /dev/null http://speed.transip.nl/10mb.bin"
-alias pinging="command ping"
-
-# other aliases
-alias tarls="tar -tvf"
-alias untar="tar -xf"
-alias biggest='du -s ./* | sort -nr | awk '\''{print $2}'\'' | xargs du -sh'
-alias dux='du -x --max-depth=1 | sort -n'
-alias linecount="grep -c '^'"
-alias timestamp="date '+%Y-%m-%d %H:%M:%S'"
-alias datestamp="date '+%Y-%m-%d'"
-alias fd='find . -type d -name'
-alias ff='find . -type f -name'
-alias dud='du -d 1 -h'
-alias duf='du -sh *'
-
 # auto-orient images based on exif tags
 alias autorotate="jhead -autorot"
-
-alias urldecode='python3 -c "import sys, urllib.parse as ul; \
-    print(ul.unquote_plus(sys.argv[1]))"'
-alias urlencode='python3 -c "import sys, urllib.parse as ul; \
-    print (ul.quote_plus(sys.argv[1]))"'
 
 # set initial working directory
 IWD=${IWD:-PWD}
