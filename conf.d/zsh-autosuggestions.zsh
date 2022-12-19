cb16088b01879b8f1cd9fbcc37cca4c839768fc3
mattmc3 <mattmc3@gmail.com>
2022-12-19 18:32:21 -0500

    Switch to zsh-users autosuggestions

---
+++ conf.d/zsh-autosuggestions.zsh
@@ -0,0 +1,7 @@
+# config zsh-users/zsh-autosuggestions
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=8'
+if [[ -n "$key_info" ]]; then
+  # vi
+  bindkey -M viins "$key_info[Control]F" vi-forward-word
+  bindkey -M viins "$key_info[Control]E" vi-add-eol
+fi
