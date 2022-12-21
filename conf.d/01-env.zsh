24f9c17607424c462f96f00f115b3e8255ee0854
mattmc3 <mattmc3@gmail.com>
2022-12-21 16:10:22 -0500

    Reorg environment vars

--- conf.d/01-env.zsh
+++ conf.d/01-env.zsh
@@ -1,25 +1,45 @@
-# https://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html
+##
+# Setup environment.
+#
+
+# XDG basedir support outlined here:
 # https://wiki.archlinux.org/index.php/XDG_Base_Directory
-# https://wiki.archlinux.org/index.php/XDG_user_directories
 
-export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
-export XDG_CACHE_HOME=${XDG_CACHE_HOME:-$HOME/.cache}
-export XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
-export XDG_RUNTIME_DIR=${XDG_RUNTIME_DIR:-$HOME/.xdg}
+# dotfiles
+export DOTFILES=${DOTFILES:-~/.config/dotfiles}
 
-if [[ "$OSTYPE" == darwin* ]]; then
-  export XDG_DESKTOP_DIR=${XDG_DESKTOP_DIR:-$HOME/Desktop}
-  export XDG_DOCUMENTS_DIR=${XDG_DOCUMENTS_DIR:-$HOME/Documents}
-  export XDG_DOWNLOAD_DIR=${XDG_DOWNLOAD_DIR:-$HOME/Downloads}
-  export XDG_MUSIC_DIR=${XDG_MUSIC_DIR:-$HOME/Music}
-  export XDG_PICTURES_DIR=${XDG_PICTURES_DIR:-$HOME/Pictures}
-  export XDG_VIDEOS_DIR=${XDG_VIDEOS_DIR:-$HOME/Videos}
-  export XDG_PROJECTS_DIR=${XDG_PROJECTS_DIR:-$HOME/Projects}
-fi
+# editors
+export EDITOR=nvim
+export VISUAL=code
+
+# go
+export GLOBALGOPATH=$HOME/Projects/golang
+export GOPATH=$GLOBALGOPATH
 
 # gpg
 export GNUPGHOME="${GNUPGHOME:-$XDG_DATA_HOME/gnupg}"
 alias gpg="${aliases[gpg]:-gpg} --homedir \"\$GNUPGHOME\""
+
+# groovy
+if [[ "$OSTYPE" == darwin* ]]; then
+  export GROOVY_HOME=$HOMEBREW_PREFIX/opt/groovy/libexec  # per homebrew
+  export GROOVY_TURN_OFF_JAVA_WARNINGS="true"
+fi
+
+# homebrew
+if [[ "$OSTYPE" == darwin* ]] && (( $+commands[brew] )); then
+  if [[ "${commands[brew]}" == "/opt/homebrew/bin/brew" ]]; then
+    HOMEBREW_PREFIX=/opt/homebrew
+  else
+    HOMEBREW_PREFIX=/usr/local
+  fi
+  export HOMEBREW_NO_ANALYTICS=1
+  export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+  export HOMEBREW_CELLAR="$HOMEBREW_PREFIX/Cellar";
+  export HOMEBREW_REPOSITORY="$HOMEBREW_PREFIX";
+  export MANPATH="$HOMEBREW_PREFIX/share/man${MANPATH+:$MANPATH}:";
+  export INFOPATH="$HOMEBREW_PREFIX/share/info:${INFOPATH:-}";
+fi
 
 # jupyter
 export JUPYTER_CONFIG_DIR="${JUPYTER_CONFIG_DIR:-$XDG_CONFIG_HOME/jupyter}"
@@ -27,6 +47,10 @@ export JUPYTER_CONFIG_DIR="${JUPYTER_CONFIG_DIR:-$XDG_CONFIG_HOME/jupyter}"
 # less
 export LESSKEY="${LESSKEY:-$XDG_CONFIG_HOME/less/lesskey}"
 export LESSHISTFILE="${LESSHISTFILE:-$XDG_CACHE_HOME/less/history}"
+[[ -f $LESSHISTFILE ]] || { mkdir -p $LESSHISTFILE:h && touch $LESSHISTFILE }
+
+# lpass
+export LPASS_AGENT_TIMEOUT="${LPASS_AGENT_TIMEOUT:-0}"
 
 # nodejs
 export NPM_CONFIG_USERCONFIG="${NPM_CONFIG_USERCONFIG:-$XDG_CONFIG_HOME/npm/npmrc}"
@@ -43,6 +67,7 @@ export PGSERVICEFILE="${PGSERVICEFILE:-$XDG_CONFIG_HOME/pg/pg_service.conf}"
 
 # readline
 export INPUTRC="${INPUTRC:-$XDG_CONFIG_HOME/readline/inputrc}"
+[[ -f $INPUTRC ]] || { mkdir -p $INPUTRC:h && touch $INPUTRC }
 
 # ruby bundler
 export BUNDLE_USER_CONFIG="${BUNDLE_USER_CONFIG:-$XDG_CONFIG_HOME/bundle}"
@@ -59,11 +84,30 @@ export RUSTUP_HOME="${RUSTUP_HOME:-$XDG_DATA_HOME/rustup}"
 
 # screen
 export SCREENRC="${SCREENRC:-$XDG_CONFIG_HOME/screen/screenrc}"
+[[ -f $SCREENRC ]] || { mkdir -p $SCREENRC:h && touch $SCREENRC }
 
 # tmux
 export TMUX_CONFIG="${TMUX_CONFIG:-$XDG_CONFIG_HOME/tmux/tmux.conf}"
+[[ -f $TMUX_CONFIG ]] || { mkdir -p $TMUX_CONFIG:h && touch $TMUX_CONFIG }
 alias tmux='tmux -f "$TMUX_CONFIG"'
 
 # wget
 export WGETRC="${WGETRC:-$XDG_CONFIG_HOME/wget/wgetrc}"
+[[ -f $WGETRC ]] || { mkdir -p $WGETRC:h && touch $WGETRC }
 alias wget="${aliases[wget]:-wget} --hsts-file=\"\$XDG_CACHE_HOME/wget/wget-hsts\""
+
+# Set $PATH.
+path=(
+  ~/bin(N)
+  $HOMEBREW_PREFIX/{bin,sbin}(N)
+  /usr/local/{bin,sbin}(N)
+  ~/.emacs.d/bin(N)
+  ~/.config/emacs/bin(N)
+  $HOMEBREW_PREFIX/opt/curl/bin(N)
+  $HOMEBREW_PREFIX/opt/go/libexec/bin(N)
+  $HOMEBREW_PREFIX/opt/ruby/bin(N)
+  $HOMEBREW_PREFIX/lib/ruby/gems/3.1.0/bin(N)
+  ~/.gem/ruby/3.1.0/bin(N)
+  $HOMEBREW_PREFIX/share/npm/bin(N)
+  $path
+)
