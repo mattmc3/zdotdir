a8c43abd284c81018bccc70ced5354e553658266
mattmc3 <mattmc3@gmail.com>
2022-12-22 14:32:50 -0500

    Don't repeat plugin-clone function

--- conf.d/02-clone.zsh
+++ conf.d/02-clone.zsh
@@ -2,37 +2,9 @@
 # Initialize Zsh for remaining config.
 #
 
-#region Plugin functions
-
-function plugin-clone {
-  # https://github.com/mattmc3/zsh_unplugged
-  emulate -L zsh; setopt local_options extended_glob no_monitor
-  local repo destdir initfile
-  local zhome=${ZDOTDIR:-${XDG_CONFIG_HOME:-~/.config}/zsh}
-  local plugext=$zhome/plugins/.external
-
-  for repo in ${(u)@}; do
-    destdir=$plugext/${repo:t}
-    initfile=$destdir/${repo:t}.plugin.zsh
-
-    if [[ ! -d $destdir ]]; then
-      echo "Cloning $repo..."
-      () {
-        git clone -q --depth 1 --recursive --shallow-submodules https://github.com/$repo $destdir
-        if [[ ! -e $initfile ]]; then
-          local -a initfiles=($destdir/*.plugin.{z,}sh(N) $destdir/*.{z,}sh{-theme,}(N))
-          (( $#initfiles )) && ln -sf "${initfiles[1]}" "$initfile"
-        fi
-      } &
-    fi
-  done
-  wait
-}
-
-#endregion
-
 #region Clone
 
+ZPLUGINDIR=$ZDOTDIR/plugins/.external
 _repos=(
   belak/zsh-utils
   mattmc3/zman
