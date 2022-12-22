3312b746cd67b8e14d58a0a598c7856e0adb84f2
mattmc3 <mattmc3@gmail.com>
2022-12-22 13:45:54 -0500

    Enhance completions plugin

--- plugins/completion/completion.plugin.zsh
+++ plugins/completion/completion.plugin.zsh
@@ -12,3 +12,11 @@ source $ZPLUGINDIR/.external/zsh-utils/completion/completion.plugin.zsh
 compstyle zshzoo
 
 zstyle ':completion:*:*:git:*' script $ZPLUGINDIR/.external/git/git-completion.bash
+
+# Compile compdump, if modified, in background to increase startup speed.
+: ${ZSH_COMPDUMP:=$XDG_CACHE_HOME/zsh/compdump}
+{
+  if [[ -s "$ZSH_COMPDUMP" && (! -s "${ZSH_COMPDUMP}.zwc" || "$ZSH_COMPDUMP" -nt "${ZSH_COMPDUMP}.zwc") ]]; then
+    zcompile "$ZSH_COMPDUMP"
+  fi
+} &!
