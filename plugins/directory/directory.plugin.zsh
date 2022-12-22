53c4feec2f11ecbb7a3d38cb1a10ba80d38685da
mattmc3 <mattmc3@gmail.com>
2022-12-22 14:07:55 -0500

    Allow duplicates on dirstack

--- plugins/directory/directory.plugin.zsh
+++ plugins/directory/directory.plugin.zsh
@@ -8,15 +8,15 @@
 # Options
 #
 
-setopt AUTO_PUSHD           # Push the old directory onto the stack on cd.
-setopt PUSHD_IGNORE_DUPS    # Do not store duplicates in the stack.
-setopt PUSHD_SILENT         # Do not print the directory stack after pushd or popd.
-setopt PUSHD_TO_HOME        # Push to home directory when no argument is given.
-setopt CDABLE_VARS          # Change directory to a path stored in a variable.
-setopt MULTIOS              # Write to multiple descriptors.
-setopt EXTENDED_GLOB        # Use extended globbing syntax.
-setopt NO_AUTO_CD           # Auto changes to a directory without typing cd.
-setopt NO_CLOBBER           # Do not overwrite existing files with > and >>. Use >! and >>! to bypass.
+setopt AUTO_PUSHD            # Push the old directory onto the stack on cd.
+setopt PUSHD_SILENT          # Do not print the directory stack after pushd or popd.
+setopt PUSHD_TO_HOME         # Push to home directory when no argument is given.
+setopt CDABLE_VARS           # Change directory to a path stored in a variable.
+setopt MULTIOS               # Write to multiple descriptors.
+setopt EXTENDED_GLOB         # Use extended globbing syntax.
+setopt NO_AUTO_CD            # Auto changes to a directory without typing cd.
+setopt NO_CLOBBER            # Do not overwrite existing files with > and >>. Use >! and >>! to bypass.
+setopt NO_PUSHD_IGNORE_DUPS  # Allow duplicates in the stack.
 
 #
 # Aliases
