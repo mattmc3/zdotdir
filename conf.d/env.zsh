e8bbada620a5dba8d8c1d9e82cd92434e6e5b716
mattmc3 <mattmc3@gmail.com>
2023-01-24 21:24:02 -0500

    Add xdg-basedir plugin

--- conf.d/env.zsh
+++ conf.d/env.zsh
@@ -12,6 +12,12 @@
 # autosuggestions
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=60'
 
+# brew (brew shellenv)
+if (( $+commands[brew] )); then
+  export HOMEBREW_NO_ANALYTICS=1
+  [[ -n "$HOMEBREW_PREFIX" ]] || HOMEBREW_PREFIX=$(brew --prefix)
+fi
+
 # dotfiles
 export DOTFILES=${DOTFILES:-~/.config/dotfiles}
 
@@ -29,82 +35,27 @@ if [[ "$OSTYPE" == darwin* ]]; then
   export GROOVY_TURN_OFF_JAVA_WARNINGS="true"
 fi
 
-# gpg
-export GNUPGHOME="${GNUPGHOME:-$XDG_DATA_HOME/gnupg}"
-alias gpg="${aliases[gpg]:-gpg} --homedir \"\$GNUPGHOME\""
-
 # history-substring-search
 HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_FOUND='bg=magenta,fg=white,bold'
 HISTORY_SUBSTRING_SEARCH_HIGHLIGHT_NOT_FOUND='bg=red,fg=white,bold'
-
-# homebrew (brew shellenv)
-if (( $+commands[brew] )); then
-  export HOMEBREW_NO_ANALYTICS=1
-  [[ -n "$HOMEBREW_PREFIX" ]] || HOMEBREW_PREFIX=$(brew --prefix)
-fi
-
-# jupyter
-export JUPYTER_CONFIG_DIR="${JUPYTER_CONFIG_DIR:-$XDG_CONFIG_HOME/jupyter}"
-
-# less
-export LESSKEY="${LESSKEY:-$XDG_CONFIG_HOME/less/lesskey}"
-export LESSHISTFILE="${LESSHISTFILE:-$XDG_CACHE_HOME/less/history}"
-[[ -f $LESSHISTFILE ]] || { mkdir -p $LESSHISTFILE:h && touch $LESSHISTFILE }
 
 # magic-enter
 MAGIC_ENTER_GIT_COMMAND='git status -sb'
 MAGIC_ENTER_OTHER_COMMAND='ls'
 
-# nodejs
-export NPM_CONFIG_USERCONFIG="${NPM_CONFIG_USERCONFIG:-$XDG_CONFIG_HOME/npm/npmrc}"
-export NODE_REPL_HISTORY="${NODE_REPL_HISTORY:-$XDG_DATA_HOME/nodejs/repl_history}"
-
-# nuget
-export NUGET_PACKAGES="${NUGET_PACKAGES:-$XDG_CACHE_HOME/NuGetPackages}"
-
 # ohmyzsh
 ZSH=$ANTIDOTE_HOME/ohmyzsh/ohmyzsh
 
-# postgres
-export PSQLRC="${PSQLRC:-$XDG_CONFIG_HOME/pg/psqlrc}"
-export PSQL_HISTORY="${PSQL_HISTORY:-$XDG_CACHE_HOME/pg/psql_history}"
-export PGPASSFILE="${PGPASSFILE:-$XDG_CONFIG_HOME/pg/pgpass}"
-export PGSERVICEFILE="${PGSERVICEFILE:-$XDG_CONFIG_HOME/pg/pg_service.conf}"
-
-# readline
-export INPUTRC="${INPUTRC:-$XDG_CONFIG_HOME/readline/inputrc}"
-[[ -f $INPUTRC ]] || { mkdir -p $INPUTRC:h && touch $INPUTRC }
-
-# ruby bundler
-export BUNDLE_USER_CONFIG="${BUNDLE_USER_CONFIG:-$XDG_CONFIG_HOME/bundle}"
-export BUNDLE_USER_CACHE="${BUNDLE_USER_CACHE:-$XDG_CACHE_HOME/bundle}"
-export BUNDLE_USER_PLUGIN="${BUNDLE_USER_PLUGIN:-$XDG_DATA_HOME/bundle}"
-
-# ruby gems
-export GEM_HOME="${GEM_HOME:-$XDG_DATA_HOME/gem}"
-export GEM_SPEC_CACHE="${GEM_SPEC_CACHE:-$XDG_CACHE_HOME/gem}"
-
-# rust
-export CARGO_HOME="${CARGO_HOME:-$XDG_DATA_HOME/cargo}"
-export RUSTUP_HOME="${RUSTUP_HOME:-$XDG_DATA_HOME/rustup}"
-
-# screen
-export SCREENRC="${SCREENRC:-$XDG_CONFIG_HOME/screen/screenrc}"
-[[ -f $SCREENRC ]] || { mkdir -p $SCREENRC:h && touch $SCREENRC }
-
-# tmux
-export TMUX_CONFIG="${TMUX_CONFIG:-$XDG_CONFIG_HOME/tmux/tmux.conf}"
-[[ -f $TMUX_CONFIG ]] || { mkdir -p $TMUX_CONFIG:h && touch $TMUX_CONFIG }
-alias tmux='tmux -f "$TMUX_CONFIG"'
-
-# wget
-export WGETRC="${WGETRC:-$XDG_CONFIG_HOME/wget/wgetrc}"
-[[ -f $WGETRC ]] || { mkdir -p $WGETRC:h && touch $WGETRC }
-alias wget="${aliases[wget]:-wget} --hsts-file=\"\$XDG_CACHE_HOME/wget/wget-hsts\""
-
-# z (rupa/z or agkozak/zsh-z)
-_Z_DATA=${XDG_DATA_HOME:-~/.local/share}/z/data
-[[ -f $_Z_DATA ]] || { mkdir -p ${_Z_DATA:h} && touch $_Z_DATA }
+# perl
+# if [[ "$OSTYPE" == darwin* ]]; then
+#   # eval "$(perl -I$XDG_DATA_HOME/perl5/lib/perl5 -Mlocal::lib=$XDG_DATA_HOME/perl5)"
+#   export PERL_MB_OPT='--install_base "$XDG_DATA_HOME/perl5"'
+#   export PERL_MM_OPT='INSTALL_BASE=$XDG_DATA_HOME/perl5'
+#   if [[ ! -d $XDG_DATA_HOME/perl5 ]]; then
+#     mkdir -p $XDG_DATA_HOME/perl5
+#     cpan local::lib
+#   fi
+# fi
 
 # zsh-abbr
 ABBR_USER_ABBREVIATIONS_FILE=$ZDOTDIR/.zabbr
---
+++ plugins/xdg-basedir/xdg-basedir.plugin.zsh
@@ -0,0 +1,82 @@
+# https://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html
+# https://wiki.archlinux.org/index.php/XDG_Base_Directory
+# https://wiki.archlinux.org/index.php/XDG_user_directories
+
+export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-$HOME/.config}
+export XDG_CACHE_HOME=${XDG_CACHE_HOME:-$HOME/.cache}
+export XDG_DATA_HOME=${XDG_DATA_HOME:-$HOME/.local/share}
+export XDG_RUNTIME_DIR=${XDG_RUNTIME_DIR:-$HOME/.xdg}
+
+for xdir in $XDG_CONFIG_HOME $XDG_CACHE_HOME $XDG_DATA_HOME $XDG_RUNTIME_DIR; do
+  [[ -d $xdir ]] || mkdir -p $xdir
+done
+unset xdir
+
+if [[ "$OSTYPE" == darwin* ]]; then
+  export XDG_DESKTOP_DIR=${XDG_DESKTOP_DIR:-$HOME/Desktop}
+  export XDG_DOCUMENTS_DIR=${XDG_DOCUMENTS_DIR:-$HOME/Documents}
+  export XDG_DOWNLOAD_DIR=${XDG_DOWNLOAD_DIR:-$HOME/Downloads}
+  export XDG_MUSIC_DIR=${XDG_MUSIC_DIR:-$HOME/Music}
+  export XDG_PICTURES_DIR=${XDG_PICTURES_DIR:-$HOME/Pictures}
+  export XDG_VIDEOS_DIR=${XDG_VIDEOS_DIR:-$HOME/Videos}
+  export XDG_PROJECTS_DIR=${XDG_PROJECTS_DIR:-$HOME/Projects}
+fi
+
+# gpg
+export GNUPGHOME="${GNUPGHOME:-$XDG_DATA_HOME/gnupg}"
+alias gpg="${aliases[gpg]:-gpg} --homedir \"\$GNUPGHOME\""
+
+# jupyter
+export JUPYTER_CONFIG_DIR="${JUPYTER_CONFIG_DIR:-$XDG_CONFIG_HOME/jupyter}"
+
+# less
+export LESSKEY="${LESSKEY:-$XDG_CONFIG_HOME/less/lesskey}"
+export LESSHISTFILE="${LESSHISTFILE:-$XDG_CACHE_HOME/less/history}"
+[[ -f $LESSHISTFILE ]] || { mkdir -p $LESSHISTFILE:h && touch $LESSHISTFILE }
+
+# nodejs
+export NPM_CONFIG_USERCONFIG="${NPM_CONFIG_USERCONFIG:-$XDG_CONFIG_HOME/npm/npmrc}"
+export NODE_REPL_HISTORY="${NODE_REPL_HISTORY:-$XDG_DATA_HOME/nodejs/repl_history}"
+
+# nuget
+export NUGET_PACKAGES="${NUGET_PACKAGES:-$XDG_CACHE_HOME/NuGetPackages}"
+
+# postgres
+export PSQLRC="${PSQLRC:-$XDG_CONFIG_HOME/pg/psqlrc}"
+export PSQL_HISTORY="${PSQL_HISTORY:-$XDG_CACHE_HOME/pg/psql_history}"
+export PGPASSFILE="${PGPASSFILE:-$XDG_CONFIG_HOME/pg/pgpass}"
+export PGSERVICEFILE="${PGSERVICEFILE:-$XDG_CONFIG_HOME/pg/pg_service.conf}"
+
+# readline
+export INPUTRC="${INPUTRC:-$XDG_CONFIG_HOME/readline/inputrc}"
+[[ -f $INPUTRC ]] || { mkdir -p $INPUTRC:h && touch $INPUTRC }
+
+# ruby bundler
+export BUNDLE_USER_CONFIG="${BUNDLE_USER_CONFIG:-$XDG_CONFIG_HOME/bundle}"
+export BUNDLE_USER_CACHE="${BUNDLE_USER_CACHE:-$XDG_CACHE_HOME/bundle}"
+export BUNDLE_USER_PLUGIN="${BUNDLE_USER_PLUGIN:-$XDG_DATA_HOME/bundle}"
+
+# ruby gems
+export GEM_HOME="${GEM_HOME:-$XDG_DATA_HOME/gem}"
+export GEM_SPEC_CACHE="${GEM_SPEC_CACHE:-$XDG_CACHE_HOME/gem}"
+
+# rust
+export CARGO_HOME="${CARGO_HOME:-$XDG_DATA_HOME/cargo}"
+export RUSTUP_HOME="${RUSTUP_HOME:-$XDG_DATA_HOME/rustup}"
+
+# screen
+export SCREENRC="${SCREENRC:-$XDG_CONFIG_HOME/screen/screenrc}"
+
+# tmux
+export TMUX_CONFIG="${TMUX_CONFIG:-$XDG_CONFIG_HOME/tmux/tmux.conf}"
+[[ -f $TMUX_CONFIG ]] || { mkdir -p $TMUX_CONFIG:h && touch $TMUX_CONFIG }
+alias tmux="${aliases[tmux]:-tmux} -f \"\$TMUX_CONFIG\""
+
+# wget
+export WGETRC="${WGETRC:-$XDG_CONFIG_HOME/wget/wgetrc}"
+[[ -f $WGETRC ]] || { mkdir -p $WGETRC:h && touch $WGETRC }
+alias wget="${aliases[wget]:-wget} --hsts-file=\"\$XDG_CACHE_HOME/wget/wget-hsts\""
+
+# z
+export _Z_DATA="${_Z_DATA:-$XDG_DATA_HOME/z/data}"
+[[ -f $_Z_DATA ]] || { mkdir -p "$_Z_DATA:h" && touch "$_Z_DATA" }
