a19b90ab2768c2b97d96d7000841874246315d74
Niclas D. Gesing <nicdomgesing@gmail.com>
2023-03-22 07:52:47 +0000

    ✨ adds fzf and calls required init-functions

--- conf.d/misc.zsh
+++ conf.d/misc.zsh
@@ -17,3 +17,29 @@ setopt NO_HIST_BEEP  # Be quiet!
 #
 
 [[ -n "$ANTIDOTE_HOME" ]] || ANTIDOTE_HOME="$(antidote home)"
+
+
+#
+# misc
+#
+
+# pyenv
+
+# export PYENV_ROOT="$HOME/.pyenv"
+# export PATH="$PYENV_ROOT/bin:$PATH"
+# eval "$(pyenv init --path)"
+
+# fzf
+
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+# local/bin
+
+export PATH="$PATH:/home/ngesing/.local/bin"
+
+# execute plugin functions
+init-zabbr
+init-zshz
+init-magic-enter
+init-autosuggestions
+init-history-substring-search
