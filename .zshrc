30574e8e7cc695ed44fb167e7abae173cc4cf288
mattmc3 <mattmc3@gmail.com>
2023-04-23 20:33:29 -0400

    Support powerlevel10k prompt

--- .zshrc
+++ .zshrc
@@ -5,6 +5,9 @@
 [[ -z "$ZPROFRC" ]] || zmodload zsh/zprof
 alias zprofrc="ZPROFRC=1 zsh"
 
+# p10 insta-prompt
+source $ZDOTDIR/plugins/p10k-instaprompt/p10k-instaprompt.plugin.zsh
+
 # init functions
 fpath+=($ZDOTDIR/functions/zshinit)
 autoload -Uz zshinit && zshinit
--- plugins/p10k-instaprompt/p10k-instaprompt.plugin.zsh
+++ plugins/p10k-instaprompt/p10k-instaprompt.plugin.zsh
@@ -1,4 +1,7 @@
-[[ ${ZPROFRC:-0} -eq 0 ]] || typeset -g POWERLEVEL9K_INSTANT_PROMPT=off
+[[ -z "$ZPROFRC" ]] || typeset -g POWERLEVEL9K_INSTANT_PROMPT=off
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.config/zsh/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
   source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
