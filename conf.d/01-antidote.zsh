c87eb7039eab10436faf08aa54747d859faa7728
mattmc3 <mattmc3@gmail.com>
2023-05-15 17:42:49 -0400

    Lazy load antidote

--- conf.d/01-antidote.zsh
+++ conf.d/01-antidote.zsh
@@ -1,7 +1,9 @@
 # Load antidote for plugin management.
-source ${HOMEBREW_PREFIX:=/opt/homebrew}/opt/antidote/share/antidote/antidote.zsh
 zplugins=${ZDOTDIR:-$HOME}/.zplugins
 if [[ ! ${zplugins}.zsh -nt ${zplugins} ]]; then
-  (antidote bundle <${zplugins} >${zplugins}.zsh)
+  (
+    source ${HOMEBREW_PREFIX:=/opt/homebrew}/opt/antidote/share/antidote/antidote.zsh
+    antidote bundle <${zplugins} >${zplugins}.zsh
+  )
 fi
 source ${zplugins}.zsh
