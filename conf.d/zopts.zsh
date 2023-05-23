19b580509a0415982cc2b9621dfc71318860acdd
mattmc3 <mattmc3@gmail.com>
2023-05-23 10:36:44 -0400

    Transient rprompt

---
+++ conf.d/zopts.zsh
@@ -0,0 +1,9 @@
+#
+# zopts - set better zsh options
+#
+
+# undo bad Prezto options
+setopt NO_beep NO_histbeep NO_share_history
+
+# remove right prompt for prior commands
+setopt transient_rprompt
