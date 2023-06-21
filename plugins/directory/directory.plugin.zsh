1a22bee2d2a5ebf71166e8491f9cf41f9f392f66
mattmc3 <mattmc3@gmail.com>
2023-06-20 23:25:43 -0400

    Update directory

--- plugins/directory/directory.plugin.zsh
+++ plugins/directory/directory.plugin.zsh
@@ -2,12 +2,31 @@
 # directory - set directory options and aliases
 #
 
-# Use Prezto directory module.
-plugin-load sorin-ionescu/prezto/modules/directory
+#
+# Options
+#
 
-# undo bad Prezto settings
-# unsetopt AUTO_CD    # Do not auto change to a directory without typing cd.
-# unalias d
+setopt auto_cd                 # Auto changes to a directory without typing cd.
+setopt auto_pushd              # Make cd push the old directory onto the dirstack.
+setopt cdable_vars             # Change directory to a path stored in a variable.
+setopt extended_glob           # Use extended globbing syntax.
+setopt glob_dots               # Don't hide dotfiles from glob patterns.
+setopt multios                 # Write to multiple descriptors.
+setopt NO_clobber              # Don't overwrite files with >. Use >| to bypass.
+setopt NO_rm_star_silent       # Ask for confirmation for `rm *' or `rm path/*'
+setopt pushd_ignore_dups       # Don’t push multiple copies of the same directory onto the dirstack.
+setopt pushd_minus             # Exchanges meanings of +/- when navigating the dirstack.
+setopt pushd_silent            # Do not print the directory stack after pushd or popd.
+setopt pushd_to_home           # Push to home directory when no argument is given.
 
-# set other directory options
-setopt PUSHD_MINUS  # Swap meanings of +/- to be more natural.
+#
+# Aliases
+#
+
+alias -- -='cd -'
+alias dirh='dirs -v'
+for index in {1..9}; do
+  alias "$index"="cd +${index}"
+  alias -g "..$index"=$(printf '../%.0s' {1..$index})
+done
+unset index
