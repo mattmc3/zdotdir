4e87a5d6e5927deae2fa71457c3cc87ac0dc704c
mattmc3 <mattmc3@gmail.com>
2023-06-21 14:16:41 -0400

    Update editor plugin

--- plugins/editor/editor.plugin.zsh
+++ plugins/editor/editor.plugin.zsh
@@ -1,28 +1,5 @@
 #
 # editor - Set keybindings.
 #
-# https://github.com/sorin-ionescu/prezto/blob/master/modules/editor/init.zsh
 
-#
-# zstyles
-#
-
-# Set the key mapping style to 'emacs' or 'vi'.
-zstyle ':prezto:module:editor' key-bindings 'emacs'
-
-# Expand ... to ../..
-zstyle ':prezto:module:editor' dot-expansion 'yes'
-
-# Set this to whatever you want to use to edit a command
-zstyle :zle:edit-command-line editor ${EDITOR:-vim}
-
-#
-# Prezto
-#
-
-# Load plugin functions.
-0=${(%):-%N}
-source ${0:A:h}/external/prezto_editor.zsh
-
-# undo bad prezto settings
-unsetopt BEEP
+plugin-load zshzoo/editor
