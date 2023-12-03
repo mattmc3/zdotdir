80dd9c3a2df75b7284a72663176e3db30b767043
mattmc3 <mattmc3@gmail.com>
2023-12-03 14:55:41 -0500

    Use p10k for a bit

--- conf.d/prompt.zsh
+++ conf.d/prompt.zsh
@@ -5,6 +5,9 @@
 # remove right prompt for prior commands
 setopt transient_rprompt
 
-# prompt
-prompt starship mmc
-#prompt p10k pure
+# initialize prompt
+autoload -Uz promptinit && promptinit
+
+# prompt starship mmc
+# prompt p10k pure
+prompt p10k mmc
