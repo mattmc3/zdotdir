9cec3959887de215d3f131d72453a5f14d4c8483
mattmc3 <mattmc3@gmail.com>
2023-12-19 12:49:11 -0500

    Update instaprompt comment

--- plugins/p10k-instaprompt/p10k-instaprompt.plugin.zsh
+++ plugins/p10k-instaprompt/p10k-instaprompt.plugin.zsh
@@ -1,4 +1,4 @@
-# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.config/zsh/.zshrc.
+# Enable Powerlevel10k instant prompt. Should stay close to the top of .zshrc.
 # Initialization code that may require console input (password prompts, [y/n]
 # confirmations, etc.) must go above this block; everything else may go below.
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
