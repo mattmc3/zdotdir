35b21fbf28519b89bd56bc6a268b28916a26cfba
mattmc3 <mattmc3@gmail.com>
2024-03-07 22:50:46 -0500

    Move some local plugins to conf.d

--- conf.d/gpg.zsh
+++ conf.d/gpg.zsh
@@ -1,7 +1,3 @@
-#
-# gpg
-#
-
 export GNUPGHOME=${GNUPGHOME:=${XDG_DATA_HOME:-$HOME/.local/share}/gnupg}
 [[ -e $GNUPGHOME:h ]] || mkdir -p $GNUPGHOME:h
 alias gpg="${aliases[gpg]:-gpg} --homedir \"$GNUPGHOME\""
---
+++ conf.d/zoxide.zsh
@@ -0,0 +1,4 @@
+# https://github.com/ajeetdsouza/zoxide
+if (( $+commands[zoxide] )); then
+  eval "$(zoxide init zsh)"
+fi
