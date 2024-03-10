d27ea65614669631dd2462b4dc813e412a0f1f66
mattmc3 <mattmc3@gmail.com>
2024-03-10 16:09:55 -0400

    functions: Split out helper functions

---
+++ lib/functions.zsh
@@ -0,0 +1,25 @@
+#
+# functions: Common helper functions
+#
+
+##? Checks if a file can be autoloaded by trying to load it in a subshell.
+function is-autoloadable {
+  ( unfunction "$1"; autoload -U +X "$1" ) &> /dev/null
+}
+
+##? Checks if a name is a command, function, or alias.
+function is-callable {
+  (( $+commands[$1] || $+functions[$1] || $+aliases[$1] || $+builtins[$1] ))
+}
+
+##? Check whether a string represents "true" (1, y, yes, t, true, o, on).
+function is-true {
+  [[ -n "$1" && "$1:l" == (1|y(es|)|t(rue|)|o(n|)) ]]
+}
+
+# OS checks.
+function is-macos  { [[ "$OSTYPE" == darwin* ]] }
+function is-linux  { [[ "$OSTYPE" == linux*  ]] }
+function is-bsd    { [[ "$OSTYPE" == *bsd*   ]] }
+function is-cygwin { [[ "$OSTYPE" == cygwin* ]] }
+function is-termux { [[ "$OSTYPE" == linux-android ]] }
