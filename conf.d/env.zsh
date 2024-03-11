233547f67e351efa815906f6eeacf53f04d7e51e
mattmc3 <mattmc3@gmail.com>
2024-03-11 18:37:43 -0400

    conf.d: Move zprofile to env

---
+++ conf.d/env.zsh
@@ -0,0 +1,34 @@
+#!/bin/zsh
+#
+# env: Set environment variables for interactive shells.
+#
+
+# Apps
+export EDITOR=hx
+export VISUAL=code
+export PAGER=less
+
+# Set the list of directories that cd searches.
+cdpath=(
+  $XDG_PROJECTS_DIR(N/)
+  $XDG_PROJECTS_DIR/mattmc3(N/)
+  $cdpath
+)
+
+# Set the list of directories that Zsh searches for programs.
+path=(
+  # core
+  $path
+
+  # emacs
+  $HOME/.emacs.d/bin(N)
+  $XDG_CONFIG_HOME/emacs/bin(N)
+
+  # apps
+  /{usr/local,opt/homebrew}/opt/curl/bin(N)
+  /{usr/local,opt/homebrew}/opt/go/libexec/bin(N)
+  /{usr/local,opt/homebrew}/share/npm/bin(N)
+  /{usr/local,opt/homebrew}/opt/ruby/bin(N)
+  /{usr/local,opt/homebrew}/lib/ruby/gems/*/bin(N)
+  $HOME/.gem/ruby/*/bin(N)
+)
