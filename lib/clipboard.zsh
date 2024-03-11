fd076bea2b3fc939102c049f8acc96353522e08a
mattmc3 <mattmc3@gmail.com>
2024-03-11 14:24:56 -0400

    clipboard: Move from utility

---
+++ lib/clipboard.zsh
@@ -0,0 +1,25 @@
+#
+# clipboard: Use common macOS clipboard commands everywhere.
+#
+
+# Ensure pbcopy/pbpaste commands exist.
+if ! (( $+commands[pbcopy] )) && ! (( $+commands[pbpaste] )); then
+  if [[ "$OSTYPE" == cygwin* ]]; then
+    alias pbcopy='tee > /dev/clipboard'
+    alias pbpaste='cat /dev/clipboard'
+  elif [[ "$OSTYPE" == linux-android ]]; then
+    alias pbcopy='termux-clipboard-set'
+    alias pbpaste='termux-clipboard-get'
+  elif (( $+commands[wl-copy] && $+commands[wl-paste] )); then
+    alias pbcopy='wl-copy'
+    alias pbpaste='wl-paste'
+  elif [[ -n $DISPLAY ]]; then
+    if (( $+commands[xclip] )); then
+      alias pbcopy='xclip -selection clipboard -in'
+      alias pbpaste='xclip -selection clipboard -out'
+    elif (( $+commands[xsel] )); then
+      alias pbcopy='xsel --clipboard --input'
+      alias pbpaste='xsel --clipboard --output'
+    fi
+  fi
+fi
