c6547fe93ebef4d7906127eab0e12e98ae48f3ac
mattmc3 <mattmc3@gmail.com>
2024-03-12 17:37:06 -0400

    zsh-syntax-highlighting: Add colors

---
+++ conf.d/zsh-syntax.highlighting.zsh
@@ -0,0 +1,11 @@
+#
+# zsh-syntax-highlighting - Snippet to config zsh-users/zsh-syntax-highlighting
+#
+
+# See available highlighters here:
+# - https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/docs/highlighters/main.md
+
+# Add highlight style for command flags (-f|--flag).
+# named color values: red,green,blue,yellow,cyan,magenta,white,black
+ZSH_HIGHLIGHT_STYLES[single-hyphen-option]=fg=cyan
+ZSH_HIGHLIGHT_STYLES[double-hyphen-option]=fg=cyan
