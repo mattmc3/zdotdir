c615fb4c608bef5a4e9d50b140453801a5f706b5
mattmc3 <mattmc3@gmail.com>
2024-03-18 21:52:13 -0400

    Move prompt logic

--- lib/zzz.zsh
+++ lib/zzz.zsh
@@ -19,7 +19,26 @@ unset _rc{,s}
 # Run this at the very end.
 function zshrc-post {
   # Init prompt.
-  (( $#prompt_themes > 0 )) || promptinit
+  if ! (( $#prompt_themes > 0 )); then
+    promptinit
+
+    # Set prompt
+    if [[ $TERM == dumb ]]; then
+      prompt 'off'
+    else
+      # Set prompt.
+      local -a prompt_argv
+      zstyle -a ':kickstart.zsh:feature:prompt' 'theme' 'prompt_argv'
+      if (( $#prompt_argv == 0 )); then
+        if (( $+commands[starship] )); then
+          prompt_argv=(starship zephyr)
+        else
+          prompt_argv=(off)
+        fi
+      fi
+      prompt "$prompt_argv[@]"
+    fi
+  fi
 
   # Init completions.
   (( $+functions[compinit] )) || mycompinit
