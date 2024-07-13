8cec0453fbb2d31ef0ed1dd460031192c57a506e
mattmc3 <mattmc3@gmail.com>
2024-07-13 18:20:56 -0400

    Update directory lib to match Zephyr

--- lib/directory.zsh
+++ lib/directory.zsh
@@ -2,33 +2,50 @@
 # directory: Set options and aliases related to Zsh directories and dirstack.
 #
 
+# References:
+# - https://github.com/sorin-ionescu/prezto/tree/master/modules/directory
+# - https://github.com/ohmyzsh/ohmyzsh/blob/master/lib/directories.zsh
+# - https://zsh.sourceforge.io/Doc/Release/Options.html#Changing-Directories
+
 # 16.2.1 Changing Directories
-setopt auto_pushd         # Make cd push the old directory onto the dirstack.
-setopt pushd_minus        # Exchanges meanings of +/- when navigating the dirstack.
-setopt pushd_silent       # Do not print the directory stack after pushd or popd.
-setopt pushd_to_home      # Push to home directory when no argument is given.
+setopt auto_pushd              # Make cd push the old directory onto the dirstack.
+setopt pushd_minus             # Exchanges meanings of +/- when navigating the dirstack.
+setopt pushd_silent            # Do not print the directory stack after pushd or popd.
+setopt pushd_to_home           # Push to home directory when no argument is given.
 
 # 16.2.3 Expansion and Globbing
-setopt extended_glob      # Use more awesome globbing features.
-setopt glob_dots          # Include dotfiles when globbing.
+setopt extended_glob           # Use more awesome globbing features.
+setopt glob_dots               # Include dotfiles when globbing.
 
 # 16.2.6 Input/Output
-setopt path_dirs          # Perform path search even on command names with slashes.
-setopt NO_clobber         # Don't overwrite files with >. Use >| to bypass.
-setopt NO_rm_star_silent  # Ask for confirmation for `rm *' or `rm path/*'
+setopt NO_clobber              # Don't overwrite files with >. Use >| to bypass.
+setopt NO_rm_star_silent       # Ask for confirmation for `rm *' or `rm path/*'
 
 # 16.2.9 Scripts and Functions
-setopt multios        # Write to multiple descriptors.
+setopt multios                 # Write to multiple descriptors.
 
 # Set directory aliases.
-alias dirh='dirs -v'
 alias -- -='cd -'
-
+alias dirh='dirs -v'
 () {
-  local index dotdots=".."
-  for index in {1..9}; do
-    alias "$index"="cd -${index}"   # dirstack aliases (eg: "3"="cd -3")
-    alias -g "..$index"="$dotdots"  # backref aliases (eg: "..3"="../../..")
+  local i dotdots=".."
+  for i in {1..9}; do
+    alias "$i"="cd -${i}"       # dirstack aliases (eg: "3"="cd -3")
+    alias -g "..$i"="$dotdots"  # backref aliases (eg: "..3"="../../..")
     dotdots+='/..'
   done
+}
+
+##? up: Quickly go up any number of directories.
+function up {
+  local parents=${1:-1}
+  if [[ ! "$parents" -gt 0 ]]; then
+    echo >&2 "usage: up [<num>]"
+    return 1
+  fi
+  local dotdots=".."
+  while (( --parents )); do
+    dotdots+="/.."
+  done
+  cd $dotdots
 }
