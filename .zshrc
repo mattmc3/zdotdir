035534dfbe5e5c8ea66fa2c8b66a0de5ce3ce5bb
mattmc3 <mattmc3@gmail.com>
2024-07-13 00:12:19 -0400

    Move to lib

--- .zshrc
+++ .zshrc
@@ -3,31 +3,23 @@
 # .zshrc - Run on interactive Zsh session.
 #
 
-# If you are using Powerlevel10k prompt, and want to use instant prompt, uncomment this.
-# Enable Powerlevel10k instant prompt. Should stay close to the top of .zshrc.
-# Initialization code that may require console input (password prompts, [y/n]
-# confirmations, etc.) must go above this block; everything else may go below.
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
-
-# Load all the kickstart.zsh libs.
+# Load everything from lib.
 for _zrc in $ZDOTDIR/lib/*.zsh; source $_zrc; unset _zrc
 
 # Add more zsh config here, or in conf.d...
 # ...
 
-# Uncomment to manually initialize completion system if you want, or let kickstart.zsh
+# Uncomment to manually initialize completion system if you want, or let zshrc-post
 # do it automatically.
 # ZSH_COMPDUMP=${XDG_CACHE_HOME:-$HOME/.cache}/zsh/compdump
 # [[ -d $ZSH_COMPDUMP:h ]] || mkdir -p $ZSH_COMPDUMP:h
 # autoload -Uz compinit && compinit -i -d $ZSH_COMPDUMP
 
-# Uncomment to manually set your prompt, or let kickstart.zsh do it automatically.
+# Uncomment to manually set your prompt, or let zshrc-post do it automatically.
 autoload -Uz promptinit && promptinit
 prompt p10k mmc
 
-# IMPORTANT: Run zshrc-post at the very end of your .zshrc. If you don't, kickstart.zsh
+# IMPORTANT: Run zshrc-post at the very end of your .zshrc. If you don't, lib/zzz.zsh
 # will try to run it for you via a precmd hook, which might work fine, but might
 # cause problems with certain plugins/prompts (eg: Powerlevel10k).
 zshrc-post
--- lib/__init__.zsh
+++ lib/__init__.zsh
@@ -17,7 +17,41 @@ setopt extended_glob
   local _zdir; for _zdir in $@; [ -d ${(P)_zdir} ] || mkdir -p ${(P)_zdir}
 } __zsh_{config,cache,user_data}_dir
 
-# Define critical functions
+# Load .zstyles file.
+[[ -r ${ZDOTDIR:-$HOME}/.zstyles ]] && source ${ZDOTDIR:-$HOME}/.zstyles
+
+# Instant prompt
+if zstyle -t ':zdotdir:feature:prompt:powerlevel10k' instant-prompt; then
+  # Enable Powerlevel10k instant prompt. Should stay close to the top of .zshrc.
+  # Initialization code that may require console input (password prompts, [y/n]
+  # confirmations, etc.) must go above this block; everything else may go below.
+  if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+    source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+  fi
+fi
+
+# 16.2.6 Input/Output
+setopt interactive_comments    # Enable comments in interactive shell.
+setopt rc_quotes               # Allow 'Hitchhikers''s Guide' instead of 'Hitchhikers'\''s Guide'.
+setopt NO_flow_control         # Disable start/stop characters (usually ^Q/^S) in shell editor.
+setopt NO_mail_warning         # Don't print a warning message if a mail file has been accessed.
+
+# 16.2.7 Job Control
+setopt auto_resume             # Attempt to resume existing job before creating a new process.
+setopt long_list_jobs          # List jobs in the long format by default.
+setopt notify                  # Report status of background jobs immediately.
+setopt NO_bg_nice              # Don't run all background jobs at a lower priority.
+setopt NO_check_jobs           # Don't report on jobs when shell exit.
+setopt NO_hup                  # Don't kill jobs on shell exit.
+
+# 16.2.12 Zle
+setopt combining_chars         # Combine 0-len chars with the base character (eg: accents).
+setopt NO_beep                 # Beep on error in line editor.
+
+# Allow mapping Ctrl+S and Ctrl+Q shortcuts
+[[ -r ${TTY:-} && -w ${TTY:-} && $+commands[stty] == 1 ]] && stty -ixon <$TTY >$TTY
+
+# Define functions
 ##? Cache the results of an eval command
 function cached-eval {
   emulate -L zsh; setopt local_options extended_glob
@@ -33,12 +67,6 @@ function cached-eval {
   fi
   source $cachefile
 }
-
-# Load .zstyles file.
-[[ -r ${ZDOTDIR:-$HOME}/.zstyles ]] && source ${ZDOTDIR:-$HOME}/.zstyles
-
-# Load .zshrc.pre file.
-[[ -r ${ZDOTDIR:-$HOME}/.zshrc.pre ]] && source ${ZDOTDIR:-$HOME}/.zshrc.pre
 
 # Ensure path arrays do not contain duplicates.
 typeset -gU cdpath fpath mailpath path
@@ -68,3 +96,6 @@ path=(
   /usr/local/{,s}bin(N)
   $path
 )
+
+# Load .zshrc.pre file.
+[[ -r ${ZDOTDIR:-$HOME}/.zshrc.pre ]] && source ${ZDOTDIR:-$HOME}/.zshrc.pre
