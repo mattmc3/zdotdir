dcceadc12b5d89a9a9739ace31e74ee7d0c93a1d
mattmc3 <mattmc3@gmail.com>
2024-07-15 11:41:14 -0400

    Update from zephyr

--- lib/archive/__init__.zsh
+++ lib/archive/__init__.zsh
@@ -18,23 +18,6 @@ typeset -gx __zsh_{config,cache,user_data}_dir
   local _zdir; for _zdir in $@; [ -d ${(P)_zdir} ] || mkdir -p ${(P)_zdir}
 } __zsh_{config,cache,user_data}_dir
 
-##? Cache the results of an eval command
-function cached-eval {
-  emulate -L zsh; setopt local_options extended_glob
-  (( $# >= 2 )) || return 1
-
-  : ${__zsh_cache_dir:=${XDG_CACHE_HOME:-$HOME/.cache}/zsh}
-  local cmdname=$1; shift
-  local cachefile=$__zsh_cache_dir/cached/${cmdname}.zsh
-  local -a cached=($cachefile(Nmh-20))
-  # If the file has no size (is empty), or is older than 20 hours re-gen the cache.
-  if [[ ! -s $cachefile ]] || (( ! ${#cached} )); then
-    mkdir -p ${cachefile:h}
-    "$@" >| $cachefile
-  fi
-  source $cachefile
-}
-
 # Ensure path arrays do not contain duplicates.
 typeset -gU cdpath fpath mailpath path
 
@@ -55,3 +38,45 @@ path=(
 
 # Support for hooks.
 autoload -Uz add-zsh-hook
+
+##? Cache the results of an eval command
+function cached-eval {
+  emulate -L zsh; setopt local_options extended_glob
+  (( $# >= 2 )) || return 1
+
+  : ${__zsh_cache_dir:=${XDG_CACHE_HOME:-$HOME/.cache}/zsh}
+  local cmdname=$1; shift
+  local cachefile=$__zsh_cache_dir/cached/${cmdname}.zsh
+  local -a cached=($cachefile(Nmh-20))
+  # If the file has no size (is empty), or is older than 20 hours re-gen the cache.
+  if [[ ! -s $cachefile ]] || (( ! ${#cached} )); then
+    mkdir -p ${cachefile:h}
+    "$@" >| $cachefile
+  fi
+  source $cachefile
+}
+
+##? Checks if a file can be autoloaded by trying to load it in a subshell.
+function is-autoloadable {
+  ( unfunction "$1"; autoload -U +X "$1" ) &> /dev/null
+}
+
+##? Checks if a name is a command, function, or alias.
+function is-callable {
+  (( $+commands[$1] || $+functions[$1] || $+aliases[$1] || $+builtins[$1] ))
+}
+
+##? Check whether a string represents "true" (1, y, yes, t, true, o, on).
+function is-true {
+  [[ -n "$1" && "$1:l" == (1|y(es|)|t(rue|)|o(n|)) ]]
+}
+
+# OS checks.
+function is-macos  { [[ "$OSTYPE" == darwin* ]] }
+function is-linux  { [[ "$OSTYPE" == linux*  ]] }
+function is-bsd    { [[ "$OSTYPE" == *bsd*   ]] }
+function is-cygwin { [[ "$OSTYPE" == cygwin* ]] }
+function is-termux { [[ "$OSTYPE" == linux-android ]] }
+
+# Mark this plugin as loaded.
+zstyle ':zephyr:plugin:helper' loaded 'yes'
