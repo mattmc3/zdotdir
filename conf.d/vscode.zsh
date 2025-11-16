b90f2c6d91474861fbd9a7ed14100fb67c35e0af
mattmc3 <mattmc3@gmail.com>
2025-11-16 14:52:33 -0500

    Fix bad behavior by vscode integration

--- conf.d/vscode.zsh
+++ conf.d/vscode.zsh
@@ -2,7 +2,10 @@
 # vscode
 #
 
-if [[ "$TERM_PROGRAM" == "vscode" ]]; then
+if [[ "${TERM_PROGRAM:l}" == "vscode" ]]; then
   # https://code.visualstudio.com/docs/terminal/shell-integration
+  MY_HISTFILE=$HISTFILE
   source "$(code --locate-shell-integration-path zsh)"
+  HISTFILE=$MY_HISTFILE
+  unset MY_HISTFILE
 fi
