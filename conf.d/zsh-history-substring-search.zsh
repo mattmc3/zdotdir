24d13c0d211478afc8dbdd67f565968fe82af67e
mattmc3 <mattmc3@gmail.com>
2025-12-12 08:41:00 -0500

    Bugfix: var name

--- conf.d/zsh-history-substring-search.zsh
+++ conf.d/zsh-history-substring-search.zsh
@@ -45,9 +45,9 @@ bindkey -M vicmd "k" history-substring-search-up
 bindkey -M vicmd "j" history-substring-search-down
 
 # Emacs and Vi
-for _keymap in 'emacs' 'viins'; do
-  bindkey -M "$keymap" "$terminfo[kcuu1]" history-substring-search-up
-  bindkey -M "$keymap" "$terminfo[kcud1]" history-substring-search-down
+for _keymap in 'main' 'emacs' 'viins'; do
+  bindkey -M "$_keymap" "$terminfo[kcuu1]" history-substring-search-up
+  bindkey -M "$_keymap" "$terminfo[kcud1]" history-substring-search-down
 done
 
 unset _keymap
