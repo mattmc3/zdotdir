9fb944c20b13e22be8c23aa20543eabe76579abc
mattmc3 <mattmc3@gmail.com>
2025-12-23 10:55:26 -0500

    Add terminal script that consolidates the others

---
+++ conf.d/terminal.zsh
@@ -0,0 +1,26 @@
+# Terminal
+
+case "${TERM_PROGRAM:l}" in
+  apple_terminal)
+    export SHELL_SESSIONS_DISABLE=1
+    ;;
+  ghostty)
+    source ${GHOSTTY_RESOURCES_DIR}/shell-integration/zsh/ghostty-integration
+    ;;
+  vscode)
+    # https://code.visualstudio.com/docs/terminal/shell-integration
+    MY_HISTFILE=$HISTFILE
+    source "$(code --locate-shell-integration-path zsh)"
+    HISTFILE=$MY_HISTFILE
+    unset MY_HISTFILE
+    ;;
+  wezterm)
+    source "$ZDOTDIR/lib/wezterm-shell-integration.sh"
+    function set_current_shell() {
+      __wezterm_set_user_var "TERM_CURRENT_SHELL" "zsh ${${ZSH_PATCHLEVEL:-$ZSH_VERSION}#zsh-}"
+    }
+    set_current_shell
+    autoload -Uz add-zsh-hook
+    add-zsh-hook precmd set_current_shell
+    ;;
+esac
