56724cabfe637f6739a01636e462f4922bc42c90
mattmc3 <mattmc3@gmail.com>
2026-03-27 13:38:05 -0400

    Update antidote

--- lib/antidote.zsh
+++ lib/antidote.zsh
@@ -5,15 +5,16 @@
 : ${ANTIDOTE_HOME:=${XDG_CACHE_HOME:-~/.cache}/repos}
 
 # Keep all of these for different test scenarios.
-# ANTIDOTE_REPO=$ANTIDOTE_HOME/github.com/mattmc3/antidote
-ANTIDOTE_REPO=$ANTIDOTE_HOME/https-COLON--SLASH--SLASH-github.com-SLASH-mattmc3-SLASH-antidote
+ANTIDOTE_REPO=$ANTIDOTE_HOME/github.com/mattmc3/antidote
+# ANTIDOTE_REPO=$ANTIDOTE_HOME/https-COLON--SLASH--SLASH-github.com-SLASH-mattmc3-SLASH-antidote
 # ANTIDOTE_REPO=~/Projects/mattmc3/antidote
 # ANTIDOTE_REPO=${HOMEBREW_PREFIX:-/opt/homebrew}/opt/antidote/share/antidote
 
 zstyle ':antidote:home' path $ANTIDOTE_HOME
 zstyle ':antidote:repo' path $ANTIDOTE_REPO
 # zstyle ':antidote:bundle' use-friendly-names 'yes'
-zstyle ':antidote:bundle' path-style escaped
+# zstyle ':antidote:bundle' path-style escaped
+zstyle ':antidote:bundle' path-style full
 zstyle ':antidote:plugin:*' defer-options '-p'
 zstyle ':antidote:*' zcompile 'yes'
 
@@ -21,6 +22,11 @@ zstyle ':antidote:*' zcompile 'yes'
 if [[ ! -d $ANTIDOTE_REPO ]]; then
   git clone https://github.com/mattmc3/antidote $ANTIDOTE_REPO
 fi
+
+function antidote_reset() {
+  rm ${ZDOTDIR:-$HOME}/.zsh_plugins.zsh
+  rm -rf -- "${ANTIDOTE_HOME:-?}"
+}
 
 # Set the root name of the plugins files (.txt and .zsh) antidote will use.
 zsh_plugins=${ZDOTDIR:-$HOME}/.zsh_plugins
