c7a4fbaccb50aebb4280f33840210aa0da89fdb4
mattmc3 <mattmc3@gmail.com>
2026-03-27 16:28:04 -0400

    Update prj

--- conf.d/prj.zsh
+++ conf.d/prj.zsh
@@ -2,4 +2,8 @@
 # prj: Project jumper
 #
 
-eval "$("$ZSH_CONFIG_DIR/bin/prj" -i zsh)"
+if [[ -e /opt/homebrew/bin/prj ]]; then
+  eval "$(/opt/homebrew/bin/prj -i zsh)"
+elif [[ -e $HOME/bin/prj ]]; then
+  eval "$("$HOME/bin/prj" -i zsh)"
+fi
