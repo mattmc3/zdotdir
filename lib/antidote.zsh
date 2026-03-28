6fd2dbc0e692b80c5d4011fa8873ae8efda65aa2
mattmc3 <mattmc3@gmail.com>
2026-03-27 20:14:24 -0400

    Update antidote

--- lib/antidote.zsh
+++ lib/antidote.zsh
@@ -34,14 +34,17 @@ zsh_plugins=${ZDOTDIR:-$HOME}/.zsh_plugins
 # Ensure the .zsh_plugins.txt file exists so you can add plugins.
 [[ -f ${zsh_plugins}.txt ]] || touch ${zsh_plugins}.txt
 
-# Lazy-load antidote from its functions directory.
-fpath=($ANTIDOTE_REPO $fpath)
-autoload -Uz antidote
+source $ANTIDOTE_REPO/antidote.zsh
+antidote load
 
-# Generate a new static file whenever .zsh_plugins.txt is updated.
-if [[ ! ${zsh_plugins}.zsh -nt ${zsh_plugins}.txt ]]; then
-  antidote bundle <${zsh_plugins}.txt >|${zsh_plugins}.zsh
-fi
+# # Lazy-load antidote from its functions directory.
+# fpath=($ANTIDOTE_REPO $fpath)
+# autoload -Uz antidote
 
-# Source your static plugins file.
-source ${zsh_plugins}.zsh
+# # Generate a new static file whenever .zsh_plugins.txt is updated.
+# if [[ ! ${zsh_plugins}.zsh -nt ${zsh_plugins}.txt ]]; then
+#   antidote bundle <${zsh_plugins}.txt >|${zsh_plugins}.zsh
+# fi
+
+# # Source your static plugins file.
+# source ${zsh_plugins}.zsh
