647b87c682a6febf82fcb87869295246dbb9fe2d
mattmc3 <mattmc3@gmail.com>
2026-08-05 15:58:35 -0400

    feat(p10k): add instaprompt to lib

---
+++ lib/p10k-instaprompt.zsh
@@ -0,0 +1,6 @@
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.config/zsh/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
